09fbf0c6c2277cc284147dd193872655eaa93983
Rakitzis45 <michael.rakitzis@gmail.com>
2021-04-15 11:30:43 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_143441) do
+ActiveRecord::Schema.define(version: 2021_04_15_151732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.string "github_link"
+    t.string "youtube_demo"
+    t.text "project_description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_projects", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_user_projects_on_project_id"
+    t.index ["user_id"], name: "index_user_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -29,4 +47,6 @@ ActiveRecord::Schema.define(version: 2021_04_14_143441) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "user_projects", "projects"
+  add_foreign_key "user_projects", "users"
 end
