015f44d20b3cda03d54815f23a5568e7e36ec9a0
turpp <white.tripp@icloud.com>
2021-04-21 09:52:43 -0500

    actions for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    def create
+
+    end
+
+    def show
+        byebug
+        user = User.find_by(id: params[:id])
+        render json: user
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
 end
