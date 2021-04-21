eb01733577322e7bd7a7035db8c0c8e76e940598
turpp <white.tripp@icloud.com>
2021-04-21 10:55:20 -0500

    added helpers for logged_in and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    # skip_before_action :verify_authenticity_token
+    helper_method :logged_in?, :current_user
+
+    def logged_in?
+        !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:id]) if session[:id]
+    end
 end
