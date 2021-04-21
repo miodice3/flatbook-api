00db71727e19837709547b1e835a1ced317e9f77
turpp <white.tripp@icloud.com>
2021-04-21 10:55:48 -0500

    added logged_in route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :user_projects
   resources :projects
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/logged_in', to: 'sessions#logged_in'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
