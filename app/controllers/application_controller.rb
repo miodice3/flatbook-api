62b15cd4f53dd17c9c2253f8f7a6a4b94c981ce1
turpp <white.tripp@icloud.com>
2021-04-22 10:14:30 -0500

    contorller skips auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    # skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
     helper_method :logged_in?, :current_user
 
     def logged_in?
