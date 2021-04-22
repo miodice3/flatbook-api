ee14653f4af18d61c6c33f927ae6cc8533231aba
turpp <white.tripp@icloud.com>
2021-04-22 10:13:55 -0500

    added routes for sessions logging in and out

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   resources :users
 
   get '/logged_in', to: 'sessions#logged_in'
+  post '/sessions', to: 'sessions#create'
+  delete '/logout', to: 'sessions#logout'
+
+  
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
