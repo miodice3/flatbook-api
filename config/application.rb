151c38514dc3887d39569d26697cb03e1a274a24
turpp <white.tripp@icloud.com>
2021-04-23 12:31:49 -0500

    added middle ware for cookies and session and changed api_only to false

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module MyApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -32,6 +35,6 @@ module MyApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
   end
 end
