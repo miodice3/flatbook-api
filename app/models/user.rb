5b9ad502c48e1fd9db9e94b1b380147d10d6ea08
turpp <white.tripp@icloud.com>
2021-04-23 12:31:25 -0500

    removed phone requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :user_projects
     has_many :projects, through: :user_projects
-    validates_presence_of :name, :phone, :email, :password
+    validates_presence_of :name, :email, :password
     validates_uniqueness_of :email
     has_secure_password
 end
